086ad93a7fb99d0ca0c04d8f39afbb15eac7cca9
Your Name <you@example.com>
2023-08-07 18:35:46 +0300

    Задача 1 решена

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Задачи записаны в ветки:
+// Задача 1 - ветка main;
+// Задача 2 - ветка vetka2;
+// Задача 3 - ветка vetka3;
+// Задача 4 - ветка vetka4;
+
+
+//Программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+int num1=int.Parse(Console.ReadLine());
+int num2=int.Parse(Console.ReadLine());
+if (num1>num2)
+   Console.WriteLine("Максимальное число: num1="+num1); 
+else
+    Console.WriteLine("Максимальное число: - num2="+num2); 
